3aad7d5131b33e722de4cd04691b92bd5c269b28
LAXMENDRA GURJAR <51027996+VICTOR4046@users.noreply.github.com>
2020-07-25 18:14:17 +0530

    Update home.dart

--- StudyMate/lib/Screens/home/home.dart
+++ StudyMate/lib/Screens/home/home.dart
@@ -1,27 +1,23 @@
-import 'package:StudyMate/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:StudyMate/Screens/home/screens/welcome_screen.dart';
 
 class Home extends StatelessWidget {
-  final AuthService _auth = AuthService();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('StudyMate'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Logut'),
-            onPressed: () async {
-              await _auth.SignOut();
-            },
-          )
-        ],
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'StudyMate',
+      theme: ThemeData(
+        primaryColor: Color(0xFF202328),
+        accentColor: Colors.blueAccent,
+        backgroundColor: Color(0xFF12171D),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: WelcomeScreen(),
     );
   }
 }
