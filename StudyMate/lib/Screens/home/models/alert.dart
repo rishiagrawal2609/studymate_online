043d03ceb671b6d279ed36cc1d79318455c77a2a
LAXMENDRA GURJAR <51027996+VICTOR4046@users.noreply.github.com>
2020-07-27 16:14:03 +0530

    Update alert.dart

--- StudyMate/lib/Screens/home/models/alert.dart
+++ StudyMate/lib/Screens/home/models/alert.dart
@@ -10,16 +10,16 @@ List<Alert> recentAlerts = [
   Alert(
     title: 'Maths Test',
     subject: 'Linear Algebra',
-    time: DateTime.parse('2020-07-28 12:30:00'),
+    time: DateTime.parse('2020-07-30 12:30:00'),
   ),
   Alert(
     title: 'Physics Test',
     subject: 'Kinematics',
-    time: DateTime.parse('2020-07-30 14:30:00'),
+    time: DateTime.parse('2020-07-28 14:30:00'),
   ),
   Alert(
     title: 'Chemistry Test',
     subject: 'Organic',
-    time: DateTime.parse('2020-07-24 14:30:00'),
+    time: DateTime.parse('2020-07-26 14:30:00'),
   ),
 ];
