8c15c96d60fd4ad26461b02cdddba17e6fbd9428
LAXMENDRA GURJAR <51027996+VICTOR4046@users.noreply.github.com>
2020-07-27 16:12:33 +0530

    Update sign_in.dart

--- StudyMate/lib/Screens/authenticate/sign_in.dart
+++ StudyMate/lib/Screens/authenticate/sign_in.dart
@@ -19,20 +19,11 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[100],
+      backgroundColor: Color(0xFF282B30),
       appBar: AppBar(
-        backgroundColor: Colors.brown[400],
+        backgroundColor: Color(0xFF6C7174),
         elevation: 0.0,
         title: Text('Sign In To StudyMate'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
@@ -43,18 +34,25 @@ class _SignInState extends State<SignIn> {
               SizedBox(height: 20.0),
               TextFormField(
                   decoration: InputDecoration(
-                    hintText: 'Email',
-                  ),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                          borderRadius: BorderRadius.circular(20))),
                   onChanged: (val) {
                     // ignore: unused_label
                     validator:
                     (val) => val.isEmpty ? 'Enter an Email' : null;
                     setState(() => email = val);
                   }),
-              SizedBox(height: 20.0),
+              SizedBox(height: 25.0),
               TextFormField(
                   decoration: InputDecoration(
                     hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blueAccent),
+                        borderRadius: BorderRadius.circular(20)),
                   ),
                   obscureText: true,
                   onChanged: (val) {
@@ -66,11 +64,14 @@ class _SignInState extends State<SignIn> {
 
                     setState(() => password = val);
                   }),
-              SizedBox(height: 20.0),
+              SizedBox(height: 30.0),
               RaisedButton(
-                color: Colors.pink[400],
+                color: Colors.blueAccent,
+                splashColor: Colors.grey,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
                 child: Text(
-                  'SignIn',
+                  'Sign In',
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
@@ -83,11 +84,37 @@ class _SignInState extends State<SignIn> {
                   }
                 },
               ),
-              SizedBox(height: 12.0),
+              SizedBox(height: 20.0),
               Text(
                 error,
                 style: TextStyle(color: Colors.red, fontSize: 14.0),
               ),
+              Text(
+                'OR',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                label: Text('Sign Up'),
+                textColor: Colors.white,
+                color: Colors.greenAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                onPressed: () {
+                  widget.toggleView();
+                },
+              ),
+              SizedBox(height: 60.0),
+              Text(
+                'Check your homeworks or assignments, Exam live time alert, Attend online classes, check your exam dates, Attend quizzes and much more!',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800),
+              )
             ],
           ),
         ),
