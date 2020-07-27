8e9b574ada3c841771d94d81d6df6b0397333093
LAXMENDRA GURJAR <51027996+VICTOR4046@users.noreply.github.com>
2020-07-27 16:20:32 +0530

    Create about.dart

---
+++ StudyMate/lib/Screens/home/screens/about.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:StudyMate/Screens/home/constants.dart';
+import 'package:StudyMate/Screens/home/widgets/bottom_navigation.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height - 100,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Theme.of(context).backgroundColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50.0),
+                  bottomRight: Radius.circular(50.0),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 200.0,
+              left: 100.0,
+              right: 100.0,
+              child: Column(
+                children: <Widget>[
+                  ClipRRect(
+                    child: SvgPicture.asset(
+                      'assets/icons/grad_cap.png',
+                      height: 180.0,
+                      width: 180.0,
+                    ),
+                  ),
+                  Text(
+                    'StudyMate',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 170.0,
+              left: 50.0,
+              right: 50.0,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.1,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Check your assignments, Attend online classes, and check your exam dates!',
+                    style: TextStyle(
+                      color: kTextColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                    maxLines: 3,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height - 130,
+              left: 100.0,
+              right: 100.0,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => BottomNavigation()),
+                  );
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 55.0,
+                  padding: EdgeInsets.only(left: 40.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Theme.of(context).accentColor,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: Text(
+                          'GET STARTED',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Icon(
+                        Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                        size: 28.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
